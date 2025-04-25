679154f1058e2b5db2de1eeac35b9ce8cdb832e8
Charlie Schwartz <schwartz01@allegheny.edu>
2025-04-25 10:34:45 -0400

    fix: fix about me image

--- nav-script.js
+++ nav-script.js
@@ -1,29 +1,3 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('menu').classList.toggle('active');
 });
-
-// function postToGoogle() {
-//     var field1 = $("#Name").val();
-//     var field2 = $("#Email").val();
-//     var field3 = $("#Message").val();
-
-//     $.ajax({
-//       url: "{https://docs.google.com/forms/u/2/d/1BblStw2qB-6aHH3NPaW0AHET8f-25otlmrB7RQ9GdPc/previewResponse",
-        
-//       //add your google form generated numbers below which are also the 'names' of your inputs     
-//       data: {
-//         "entry.903650470": field1,
-//         "entry.1849304596": field2,
-//         "entry.903650470": field3,
-//       },
-//       type: "POST",
-//       dataType: "xml",
-//       success: function (d) {
-//         $('#contact').trigger('reset');
-//       },
-//       error: function (x, y, z) {
-//         $('#contact').trigger('reset');
-//       }
-//     });
-//     return false;
-//   }

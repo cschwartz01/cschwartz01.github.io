eec844fe9c999ffcfa83c794e8ab7d6f5636aedf
Charlie Schwartz <charlieschwartz513@gmail.com>
2026-03-12 12:07:15 -0400

    add portfolio template + scroll gallery

--- nav-script.js
+++ nav-script.js
@@ -1,3 +1,22 @@
+const gallery = document.getElementById("galleryTrack");
+
+const leftBtn = document.querySelector(".gallery-btn.left");
+const rightBtn = document.querySelector(".gallery-btn.right");
+
+leftBtn.addEventListener("click", () => {
+  gallery.scrollBy({
+    left: -300,
+    behavior: "smooth"
+  });
+});
+
+rightBtn.addEventListener("click", () => {
+  gallery.scrollBy({
+    left: 300,
+    behavior: "smooth"
+  });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
